6db4d989dbc0b3cceef059500d9cd0672d7b98d9
vaske454 <vasilijetomovic98@gmail.com>
2024-02-25 16:40:21 +0100

    Added favicon.

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -4,7 +4,9 @@ import WordInput from '../Components/WordInput';
 export default function Welcome() {
     return (
         <>
-            <Head title="Welcome"/>
+            <Head title="Welcome">
+                <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+            </Head>
             <div className="welcome-container">
                 <h1 className="welcome-title">Welcome to the Word Game!</h1>
                 <div className="scoring-rules-container">
